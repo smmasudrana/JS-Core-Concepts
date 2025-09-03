99fb546c02e741e9a000703c19fac2d9896f0d30
smmasudrana <smmasudrana62@gmail.com>
2025-09-03 23:21:15 +0600

    add double equal and triple equal

---
+++ double-equal.js
@@ -0,0 +1,12 @@
+//type coersion (type convert)
+console.log(2 == 2);
+console.log(2 == '2');
+console.log(2 == '12');
+console.log(true == 1);
+console.log(false == 0);
+console.log(true == '1');
+console.log(false == '0');
+console.log(null == undefined);
+console.log( NaN == NaN);
+console.log( {} == {});
+console.log( [] == []);
---
+++ triple-equal.js
@@ -0,0 +1,9 @@
+//=== first check the type . if type is different, they are different.
+//if type is same than checks the value, if value is same --> true, if value is false --> false.
+console.log(2 === 2);
+console.log(1 === 2);
+console.log(true === 2);
+console.log(false === 0);
+console.log(null === undefined);
+console.log(0 ==='');
+// console.log([] === '');
