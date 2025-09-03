76d6f54af7ea2d9d667908f16561335d8b5cbc97
smmasudrana <smmasudrana62@gmail.com>
2025-09-03 11:25:00 +0600

    add js data types

---
+++ data-type.js
@@ -0,0 +1,18 @@
+// JavaScript Data Types
+
+// 1. Primitive Data Types
+
+let a = "Hello";       // String
+let b = 42;            // Number
+let c = true;          // Boolean
+let d;                 // Undefined
+let e = null;          // Null
+let f = Symbol("id");  // Symbol (unique value)
+let g = 23n;           // BigInt (very large integers)
+
+
+// 2. Non-Primitive (Reference)
+let obj = { name: "Rafid", age: 22 }; // Object
+let arr = [1, 2, 3];                  // Array
+function greet() { return "Hi"; }     // Function
+let date = new Date();                // Built-in Object
