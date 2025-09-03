af9f3ee452b86aaeb23381eebaac26cf0b67a0c9
smmasudrana <smmasudrana62@gmail.com>
2025-09-03 11:47:03 +0600

    add undefined data type

---
+++ undefined.js
@@ -0,0 +1,22 @@
+/**
+ * undefined --> not define;
+ */
+
+let money;
+console.log(money);
+
+//function undefined;
+function total (a, b){
+    console.log('value of parameters', a, b);
+}
+total()
+
+
+//object undefined;
+const phone ={brand:'samsu', price: 12500}
+console.log(phone.color);
+
+//array undifined
+const banks = ['sonali', 'rupali', 'jomuna'];
+delete banks[1];
+console.log(banks[1]);
